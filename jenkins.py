ccf72911ba47e1a806cb87deb4baea45c5b0196a
Sudharsanan <2473982@cognizant.com>
2026-02-25 12:14:56 +0530

    Parametric

--- jenkins.py
+++ jenkins.py
@@ -1,1 +1,8 @@
-print("Hell jenkins")
+import sys
+
+# sys.argv[1] gets the parameter we passed from Jenkins
+name = sys.argv[1] if len(sys.argv) > 1 else "Unknown"
+
+print(f"Hello {name}, Jenkins is running your script!")
+
+# "C:\Users\2473982\OneDrive - Cognizant\Desktop\Jenkins\jenkins.py"
