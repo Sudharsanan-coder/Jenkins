53c5602ed9269e641d857c277ef23b7256e764c3
Sudharsanan <2473982@cognizant.com>
2026-03-04 23:17:57 +0530

    Nahhh

--- jenkins.py
+++ jenkins.py
@@ -6,5 +6,3 @@ name = sys.argv[1] if len(sys.argv) > 1 else "Unknown"
 print(f"Hello {name}, Jenkins is running your script!")
 
 # "C:\Users\2473982\OneDrive - Cognizant\Desktop\Jenkins\jenkins.py"
-
-# hello i am dev
